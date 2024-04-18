ba631f0a712c1c3ee20143bfac03ee5bf58be204
norsu <mohammedameen9011@gmail.com>
2024-04-18 12:11:19 +0530

    added pdf reader

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
 import streamlit as st
+from PyPDF2 import PdfReader
+
+def read_pdf(docs):
+    text = ""
+    for pdf in docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
 
 
 def main():
