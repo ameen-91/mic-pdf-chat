6e62a7b054eb2dd2f893f365c9425409a7517443
norsu <mohammedameen9011@gmail.com>
2024-04-18 12:00:26 +0530

    init2

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+
+def main():
+    st.set_page_config(page_title="MIC PDF Chat", page_icon=":books:")
+    st.header("PDF Chat")
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
